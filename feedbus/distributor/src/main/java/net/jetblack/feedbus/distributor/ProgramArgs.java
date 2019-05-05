92abc7600457fa64292c0aeb3ce94559a69825f2
Rob Blackbourn <rob.blackbourn@googlemail.com>
2019-05-05 08:02:46 +0100

    Used the apache commons cli

--- feedbus/distributor/src/main/java/net/jetblack/feedbus/distributor/ProgramArgs.java
+++ feedbus/distributor/src/main/java/net/jetblack/feedbus/distributor/ProgramArgs.java
@@ -3,12 +3,29 @@ package net.jetblack.feedbus.distributor;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import net.jetblack.feedbus.distributor.config.DistributorConfig;
 
 /**
  * Represents the command line arguments.
  */
 public class ProgramArgs {
+	
+	private final static String HELP_LONG_OPT = "help";
+	private final static String HOST_ARG = "h";
+	private final static String HOST_LONG_OPT = "host";
+	private final static String PORT_ARG = "p";
+	private final static String PORT_LONG_OPT = "port";
+	private final static String EVENT_QUEUE_CAPACITY_LONG_OPT = "event-queue-capacity";
+	private final static String WRITE_QUEUE_CAPACITY_LONG_OPT = "write-queue-capacity";
+	private final static String HEARTBEAT_INTERVAL_LONG_OPT = "heartbeat-interval";
 
 	private final DistributorConfig _config;
 	private final String[] _remaining;
@@ -40,68 +57,80 @@ public class ProgramArgs {
 	 * @return The parsed command line.
 	 * @throws UnknownHostException
 	 */
-	public static ProgramArgs parse(String[] args) throws UnknownHostException {
+	public static ProgramArgs parse(String[] args) {
 		
-	    String host = DistributorConfig.DEFAULT_HOST;
-	    int port = DistributorConfig.DEFAULT_PORT;
-	    int eventQueueCapacity = DistributorConfig.DEFAULT_EVENT_QUEUE_CAPACITY;
-	    int writeQueueCapacity = DistributorConfig.DEFAULT_WRITE_QUEUE_CAPACITY;
-	    long heartbeatInterval = DistributorConfig.DEFAULT_HEARTBEAT_INTERVAL;
-	    
-		int argc = 0;
-		while (argc < args.length) {
-			
-			if (!args[argc].startsWith("-")) {
-				break;
+		Options options = new Options()
+				.addOption(Option.builder()
+						.longOpt(HELP_LONG_OPT)
+						.desc("Show the usage.")
+						.build())
+				.addOption(Option.builder(HOST_ARG)
+						.longOpt(HOST_LONG_OPT)
+						.hasArg()
+						.desc("The hostname or address to bind to.")
+						.build())
+				.addOption(Option.builder(PORT_ARG)
+						.longOpt(PORT_LONG_OPT)
+						.hasArg()
+						.desc("The port to bind to.")
+						.build())
+				.addOption(Option.builder()
+						.longOpt(EVENT_QUEUE_CAPACITY_LONG_OPT)
+						.hasArg()
+						.desc("The capacity of the event queue.")
+						.build())
+				.addOption(Option.builder()
+						.longOpt(WRITE_QUEUE_CAPACITY_LONG_OPT)
+						.hasArg()
+						.desc("The capacuty of the interactor write queue.")
+						.build())
+				.addOption(Option.builder()
+						.longOpt(HEARTBEAT_INTERVAL_LONG_OPT)
+						.hasArg()
+						.desc("The interval in milliseconds of the heartbeat or 0 to disable.")
+						.build());
+		
+		CommandLineParser parser = new DefaultParser();
+
+		try {
+			CommandLine commandLine = parser.parse(options, args, false);
+
+			if (commandLine.hasOption(HELP_LONG_OPT)) {
+				throw new ParseException("Show help");
 			}
 			
-			if ("--host".equals(args[argc])) {
-				if (++argc >= args.length) {
-					Usage("--host");
-				}
-				host = args[argc];
-			} else if ("--port".equals(args[argc])) {
-				if (++argc >= args.length) {
-					Usage("--port");
-				}
-				port = Integer.parseInt(args[argc]);
-			} else if ("--event-queue-capacity".equals(args[argc])) {
-				if (++argc >= args.length) {
-					Usage("--event-queue-capacity");
-				}
-				eventQueueCapacity = Integer.parseInt(args[argc]);
-			} else if ("--write-queue-capacity".equals(args[argc])) {
-				if (++argc >= args.length) {
-					Usage("--write-queue-capacity");
-				}
-				writeQueueCapacity = Integer.parseInt(args[argc]);
-			} else if ("--heartbeat-interval".equals(args[argc])) {
-				if (++argc >= args.length) {
-					Usage("--heartbeat-interval");
-				}
-				heartbeatInterval = Long.parseLong(args[argc]);
-			}
+		    String host = commandLine.getOptionValue(HOST_LONG_OPT, DistributorConfig.DEFAULT_HOST);
+		    int port = commandLine.hasOption(PORT_LONG_OPT)
+		    		? Integer.parseInt(commandLine.getOptionValue(PORT_LONG_OPT))
+		    				: DistributorConfig.DEFAULT_PORT;
+		    int eventQueueCapacity = commandLine.hasOption(EVENT_QUEUE_CAPACITY_LONG_OPT)
+		    		? Integer.parseInt(commandLine.getOptionValue(EVENT_QUEUE_CAPACITY_LONG_OPT))
+		    				: DistributorConfig.DEFAULT_EVENT_QUEUE_CAPACITY;
+		    int writeQueueCapacity = commandLine.hasOption(WRITE_QUEUE_CAPACITY_LONG_OPT)
+		    		? Integer.parseInt(commandLine.getOptionValue(WRITE_QUEUE_CAPACITY_LONG_OPT))
+		    				: DistributorConfig.DEFAULT_WRITE_QUEUE_CAPACITY;
+		    long heartbeatInterval = commandLine.hasOption(HEARTBEAT_INTERVAL_LONG_OPT)
+		    		? Long.parseLong(commandLine.getOptionValue(HEARTBEAT_INTERVAL_LONG_OPT))
+		    				: DistributorConfig.DEFAULT_HEARTBEAT_INTERVAL;
+
+			DistributorConfig config = new DistributorConfig(
+					InetAddress.getByName(host),
+					port,
+					eventQueueCapacity,
+					writeQueueCapacity,
+					heartbeatInterval);
+
+			return new ProgramArgs(config, commandLine.getArgs());
 			
-			++argc;
+		} catch (ParseException e) {
+		   final HelpFormatter formatter = new HelpFormatter();
+		   formatter.printHelp("distributor", options, true);
+		} catch (UnknownHostException e) {
+			System.err.println("Unknown host name");
 		}
 		
-		DistributorConfig config = new DistributorConfig(
-				InetAddress.getByName(host),
-				port,
-				eventQueueCapacity,
-				writeQueueCapacity,
-				heartbeatInterval);
-		
-		String[] remaining = new String[args.length - argc];
-		for (int i = 0; i < remaining.length; ++i) {
-			remaining[i] = args[argc + i];
-		}
-		
-		return new ProgramArgs(config, remaining);
-	}
-	
-	private static void Usage(String parameter) {
-		System.out.println("Invalid argument: " + parameter);
 		System.exit(1);
+		
+		return null;
 	}
 }
